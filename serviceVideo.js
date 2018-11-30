7ba01e2d91d42cf8da4be9d93035324888a653fc
gs_nixbees <gs@nixbees.com>
2018-11-30 23:27:44 +0530

    adding for the first time

---
+++ serviceVideo.js
@@ -0,0 +1,36 @@
+function video1() {
+    document.getElementById("ourmodelimg").innerHTML="<div class=\"col-sm-12\">\n" +
+        "            <img src=\"images/role1.jpeg\" width=\"100%\">\n" +
+        "        </div>";
+}
+function video2() {
+    document.getElementById("ourmodelimg").innerHTML="<div class=\"col-sm-12\">\n" +
+        "            <img src=\"images/role2.jpeg\" width=\"100%\">\n" +
+        "        </div>";
+}
+function video3() {
+    document.getElementById("ourmodelimg").innerHTML="<div class=\"col-sm-12\">\n" +
+        "            <img src=\"images/role3.jpg\" width=\"100%\" height=\"250\">\n" +
+        "        </div>";
+}
+function video4() {
+    document.getElementById("ourmodelimg").innerHTML="<div class=\"col-sm-12\">\n" +
+        "            <img src=\"images/role4.jpg\" width=\"100%\" height=\"250\">\n" +
+        "        </div>";
+}
+function video5() {
+    document.getElementById("ourmodelimg").innerHTML="<div class=\"col-sm-12\">\n" +
+        "            <img src=\"images/role5.jpg\" width=\"100%\" height=\"250\">\n" +
+        "        </div>";
+}
+function video6() {
+    document.getElementById("ourmodelimg").innerHTML="<div class=\"col-sm-12\">\n" +
+        "            <img src=\"images/role6.jpg\" width=\"100%\" height=\"250\">\n" +
+        "        </div>";
+}
+function video7() {
+    document.getElementById("ourmodelimg").innerHTML="<div class=\"col-sm-12\">\n" +
+        "            <iframe src=\"video/3d_printing_process.mp4\" width=\"100%\"></iframe>\n" +
+        "        </div>";
+    
+}
---
+++ workshop.js
@@ -0,0 +1,136 @@
+/*** work shop script **/
+
+function workshop() {
+    document.getElementById("workshop").innerHTML="<div class=\"row\">\n" +
+        "                    <div class=\"col-md-12\">\n" +
+        "                        <h2>Embedded & Robotics </h2>\n" +
+        "                        <p class=\"page\">Nixbees  Technologies presents Embedded System & Robotics with Embedded ‘C’ using Arduino AVR microcontrollers.  AVR microcontrollers are useful for many applications. A microcontroller is essentially a tiny computer and can be programmed to do many things. Input and output are handled through the numerous pins on the microcontroller. It is possible to hook up sensors, switches, lights, motors, and many other items to the pins, which allow it a great range of uses. Thus, AVR microcontrollers are often used for prototyping and robotics projects. </p>\n" +
+        "                        <A href=\"pdf/one.pdf\" target=\"_blank\"><button class=\"btn knowmore1\" >Know More</button></A>\n" +
+        "                    </div>\n" +
+        "                </div>";
+    document.getElementById("embedded").style.backgroundColor="#FF5C03";
+    document.getElementById("em").style.color="white";
+    document.getElementById("arduino").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("ardi").style.color="#FF5C03";
+    document.getElementById("raspberry").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("resp").style.color="#FF5C03";
+    document.getElementById("iot").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("io").style.color="#FF5C03";
+    document.getElementById("3dprinter").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("3dp").style.color="#FF5C03";
+    document.getElementById("android").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("andy").style.color="#FF5C03";
+}
+function workshop1() {
+    document.getElementById("workshop").innerHTML="<div class=\"row\">\n" +
+        "                    <div class=\"col-md-12\">\n" +
+        "                        <h2>Workshop on Arduino  </h2>\n" +
+        "                        <p class=\"page\"><b>What is Arduino?</b><br>Arduino is a tool for the design and development of embedded computer systems, consisting of a simple open hardware design for a single-board microcontroller, with embedded I/O support and a standard programming language.The workshop will consist of Fundamentals of Programming Interactivity ,Creative Coding and hardware interfacing using the kit(optional).\n" +
+        "Arduino has become one of the pillars of the maker movement thanks to it's wonderful design and ease of use. With that said, learning to code and work with electronics can be a little intimidating. We're here to help! Our course will walk you through each step of the process and by the end you will be empowered with new skills, ready to build your own projects.\n</p>\n" +
+        "                        <A href=\"pdf/one.pdf\" target=\"_blank\"><button class=\"btn knowmore1\" >Know More</button></A>\n" +
+        "                    </div>\n" +
+        "                </div>";
+    document.getElementById("embedded").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("em").style.color="#FF5C03";
+    document.getElementById("arduino").style.backgroundColor="#FF5C03";
+    document.getElementById("ardi").style.color="white";
+    document.getElementById("raspberry").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("resp").style.color="#FF5C03";
+    document.getElementById("iot").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("io").style.color="#FF5C03";
+    document.getElementById("3dprinter").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("3dp").style.color="#FF5C03";
+    document.getElementById("android").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("andy").style.color="#FF5C03";
+
+}
+function workshop2() {
+    document.getElementById("workshop").innerHTML="<div class=\"row\">\n" +
+        "                    <div class=\"col-md-12\">\n" +
+        "                        <h2>RaspberryPi | Workshop on RaspberryPi 3 </h2>\n" +
+        "                        <p class=\"page\"><b>What is RaspberryPi ?</b><br>The Raspberry Pi is a low cost, credit-card sized computer that plugs into a computer monitor or TV, and uses a standard keyboard and mouse. It is a capable little device that enables people of all ages to explore computing, and to learn how to program in languages like Scratch and Python.\n" +
+        "Welcome to the Raspberry Pi Workshop! Here you'll be able to follow along with our series that covers everything you'll need to know to get started with your Raspberry Pi and start making awesome projects. This workshop will introduce you to the unexplored potential of the Raspberry Pi-the hardware, software and its applications. The Workshop is designed to cater to all kinds – be it a novice or a tech savvy hobbyist or an expert developer extraordinaire.\n</p>\n" +
+        "                        <A href=\"pdf/one.pdf\" target=\"_blank\"><button class=\"btn knowmore1\" >Know More</button></A>\n" +
+        "                    </div>\n" +
+        "                </div>";
+    document.getElementById("embedded").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("em").style.color="#FF5C03";
+    document.getElementById("arduino").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("ardi").style.color="#FF5C03";
+    document.getElementById("raspberry").style.backgroundColor="#FF5C03";
+    document.getElementById("resp").style.color="white";
+    document.getElementById("iot").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("io").style.color="#FF5C03";
+    document.getElementById("3dprinter").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("3dp").style.color="#FF5C03";
+    document.getElementById("android").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("andy").style.color="#FF5C03";
+
+}
+function workshop3() {
+    document.getElementById("workshop").innerHTML="<div class=\"row\">\n" +
+        "                    <div class=\"col-md-12\">\n" +
+        "                        <h2>LEARN DESIGN & DEVELOPMENT\n" +
+        "OF INTERNET OF THINGS \n </h2>\n" +
+        "                        <p class=\"page\">The participants of our training programmes are provided with all the physical tools required for building their projects on IoT. The kit contains WiFi IoT Development Board, Sensors, Actuators and access to the Cloud Platform. The courses are offered as self paced - online workshops which you can learn from the convenience of your home or classroom workshops at various Institutes, Tech fest's and locations near you. </p>\n" +
+        "                        <A href=\"pdf/one.pdf\" target=\"_blank\"><button class=\"btn knowmore1\" >Know More</button></A>\n" +
+        "                    </div>\n" +
+        "                </div>";
+    document.getElementById("embedded").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("em").style.color="#FF5C03";
+    document.getElementById("arduino").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("ardi").style.color="#FF5C03";
+    document.getElementById("raspberry").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("resp").style.color="#FF5C03";
+    document.getElementById("iot").style.backgroundColor="#FF5C03"
+    document.getElementById("io").style.color="white";
+    document.getElementById("3dprinter").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("3dp").style.color="#FF5C03";
+    document.getElementById("android").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("andy").style.color="#FF5C03";
+
+}
+function workshop4() {
+    document.getElementById("workshop").innerHTML="<div class=\"row\">\n" +
+        "                    <div class=\"col-md-12\">\n" +
+        "                        <h2>Hone your skills with Nixbees 3D Printer Training</h2>\n" +
+        "                        <p class=\"page\">Nixbees  believes in not only producing the best desktop 3D printers on the market, but also providing the highest level of customer support possible. As your level of sophistication with 3D printing grows, your need for continuing technical education may grow as well. Whether you are completely new to the world of 3D printing or you are ready to learn how to perform advanced repairs on your machine, we have a 3D printer training program that’s right for you. 3D Printing helps the participants understand the design, functioning and operation of a basic 3D Printer.</p>\n" +
+        "                        <A href=\"pdf/one.pdf\" target=\"_blank\"><button class=\"btn knowmore1\" >Know More</button></A>\n" +
+        "                    </div>\n" +
+        "                </div>";
+    document.getElementById("embedded").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("em").style.color="#FF5C03";
+    document.getElementById("arduino").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("ardi").style.color="#FF5C03";
+    document.getElementById("raspberry").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("resp").style.color="#FF5C03";
+    document.getElementById("iot").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("io").style.color="#FF5C03";
+    document.getElementById("3dprinter").style.backgroundColor="#FF5C03";
+    document.getElementById("3dp").style.color="white";
+    document.getElementById("android").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("andy").style.color="#FF5C03";
+
+}
+function workshop5() {
+    document.getElementById("workshop").innerHTML="<div class=\"row\">\n" +
+        "                    <div class=\"col-md-12\">\n" +
+        "                        <h2>ANDROID APPLICATION DEVELOPEMENT</h2>\n" +
+        "                        <p class=\"page\">A major share of hand-held devices runs on the Android Operating System - which is open source and customizable. Learn to develop applications in the latest operating system with practical implementation and begin managing your own cool applications like a professional at this workshop. Attendees will be provided with instructions to build and manage new cool applications.Instruction is aided with live projects which will allow students to grasp concepts of the complete mobile application development life-cycle.</p>\n" +
+        "                        <A href=\"pdf/one.pdf\" target=\"_blank\"><button class=\"btn knowmore1\" >Know More</button></A>\n" +
+        "                    </div>\n" +
+        "                </div>";
+    document.getElementById("embedded").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("em").style.color="#FF5C03";
+    document.getElementById("arduino").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("ardi").style.color="#FF5C03";
+    document.getElementById("raspberry").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("resp").style.color="#FF5C03";
+    document.getElementById("iot").style.backgroundColor="rgb(45, 45, 45)";
+    document.getElementById("io").style.color="#FF5C03";
+    document.getElementById("3dprinter").style.backgroundColor="rgb(45, 45, 45";
+    document.getElementById("3dp").style.color="#FF5C03";
+    document.getElementById("android").style.backgroundColor="#FF5C03";
+    document.getElementById("andy").style.color="white";
+
+}
